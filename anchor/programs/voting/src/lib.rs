c7cbf0c1d517dba9def0238cec11785daac0ff2d
0xSardius <0xSardius@gmail.com>
2026-03-11 20:06:46 -0400

    init commit

---
+++ anchor/programs/voting/src/lib.rs
@@ -0,0 +1,10 @@
+#![allow(clippy::result_large_err)]
+
+use anchor_lang::prelude::*;
+
+declare_id!("BuNqj2CksTP9QwbYBhgC9DhHLbtnru2x8uSyumWRuEeq");
+
+#[program]
+pub mod voting {
+    use super::*;
+}
